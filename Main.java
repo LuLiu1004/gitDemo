6a0d393a9166d0995a93288d15e06551928457c7
LuLiu1004 <luliu9087@gmail.com>
2025-02-24 01:40:54 +0000

    Added backend code to Main.java

--- Main.java
+++ Main.java
@@ -1,5 +1,6 @@
 public class Main{
 	public static void main(String[] args){
+		System.out.println("Still on main");
 		System.out.println("Hello world!");
 		System.out.println("Alternated by the vm");
 		System.out.println("Added by mac developer");
